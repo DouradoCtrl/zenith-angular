2577fa6cbdebd2a70d4fe48ee6a7f08fc7da7db7
DouradoCtrl <gdouradoestudante@gmail.com>
2026-07-24 01:17:17 -0300

    feat: atualiza app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,28 @@
+// 💡 CONCEITO ANGULAR: Instalação & Provedores Globais (app.config.ts)
+// Configura os serviços essenciais da aplicação, rotas, cliente HTTP e a biblioteca PrimeNG v20 com o tema Aura.
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 
 import Aura from '@primeuix/themes/aura';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    provideHttpClient(),
     provideRouter(routes),
     providePrimeNG({
-        theme: {
-            preset: Aura
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.app-dark-mode'
         }
+      }
     })
   ]
 };
