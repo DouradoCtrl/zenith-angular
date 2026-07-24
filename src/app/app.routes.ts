573118501146a4d300b163b43d5f0febc1a901fa
DouradoCtrl <gdouradoestudante@gmail.com>
2026-07-24 01:17:17 -0300

    feat: atualiza app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// 💡 CONCEITO ANGULAR: Rotas & Lazy Loading
+// Carregamento sob demanda usando `loadComponent`. Isso otimiza o bundle inicial da aplicação.
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboard'
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/components/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'pomodoro',
+    loadComponent: () => import('./features/pomodoro/components/pomodoro-timer/pomodoro-timer.component').then(m => m.PomodoroTimerComponent)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./features/settings/components/settings/settings').then(m => m.Settings)
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
